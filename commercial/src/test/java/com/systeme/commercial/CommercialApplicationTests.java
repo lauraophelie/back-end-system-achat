555de40197aac3a2a1a3e59122267e640ed4b7c8
Laura Ophélie <lauraophelie1@gmail.com>
2023-11-15 21:02:37 +0100

    first commit

--- commercial/src/test/java/com/systeme/commercial/CommercialApplicationTests.java
+++ commercial/src/test/java/com/systeme/commercial/CommercialApplicationTests.java
@@ -1,10 +1,10 @@
-package com.systeme.achat;
+package com.systeme.commercial;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AchatApplicationTests {
+class CommercialApplicationTests {
 
 	@Test
 	void contextLoads() {
