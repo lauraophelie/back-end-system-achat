49401855ac9ef568f99e3196cd9ca38257360958
Laura Ophélie <lauraophelie1@gmail.com>
2023-11-16 19:08:57 +0100

    classes

--- commercial/src/main/java/com/systeme/commercial/controller/BesoinController.java
+++ commercial/src/main/java/com/systeme/commercial/controller/BesoinController.java
@@ -1,5 +1,6 @@
 package com.systeme.commercial.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import com.systeme.commercial.service.BesoinService;
 
 @RestController
 @RequestMapping("/api/besoin")
+@CrossOrigin(origins = "*")
 public class BesoinController {
     BesoinService service;
 
--- commercial/src/main/java/com/systeme/commercial/controller/UtilisateurController.java
+++ commercial/src/main/java/com/systeme/commercial/controller/UtilisateurController.java
@@ -1,5 +1,6 @@
 package com.systeme.commercial.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import com.systeme.commercial.service.UtilisateurService;
 
 @RestController
 @RequestMapping("/api/login")
+@CrossOrigin(origins = "*")
 public class UtilisateurController {
     UtilisateurService service;
 
