3abc03128a70936dabc910d15cd5230053ce04c6
Laura Ophélie <lauraophelie1@gmail.com>
2023-11-16 19:39:13 +0100

    classes

--- commercial/src/main/java/com/systeme/commercial/controller/UtilisateurController.java
+++ commercial/src/main/java/com/systeme/commercial/controller/UtilisateurController.java
@@ -1,9 +1,14 @@
 package com.systeme.commercial.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.systeme.commercial.api.APIResponse;
+import com.systeme.commercial.model.Utilisateur;
 import com.systeme.commercial.service.UtilisateurService;
 
 @RestController
@@ -14,5 +19,11 @@ public class UtilisateurController {
 
     public UtilisateurController(UtilisateurService service) {
         this.service = service;
+    }
+
+    @PostMapping("/api/login/utilisateur")
+    public ResponseEntity<APIResponse> getUtilisateur(@RequestBody Utilisateur utilisateur) {
+        /// verification utilisateur
+        return null;
     }
 }
