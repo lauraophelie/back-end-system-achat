712cffd5d98234f8d9cc427a7991895b048500b6
Laura Ophélie <lauraophelie1@gmail.com>
2023-11-16 19:37:14 +0100

    classes

---
+++ commercial/src/main/java/com/systeme/commercial/api/ListeAPI.java
@@ -0,0 +1,13 @@
+package com.systeme.commercial.api;
+
+public class ListeAPI extends APIResponse {
+    int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
